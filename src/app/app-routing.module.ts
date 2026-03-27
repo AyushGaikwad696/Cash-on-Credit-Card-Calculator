50ee6dd9a12b6b734d97a261669b4fc686e3f362
Ayush <ayush@geotrackers.com>
2026-03-27 23:04:17 +0530

    css height for the mobile browser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { CalculatorComponent } from './features/calculator/calculator.component';
+import { CalculatorComponent } from "./features/calculator/calculator.component";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'calculator'
+    path: "",
+    pathMatch: "full",
+    redirectTo: "calculator",
   },
   {
-    path: 'calculator',
-    component: CalculatorComponent
+    path: "calculator",
+    component: CalculatorComponent,
   },
   {
-    path: '**',
-    redirectTo: 'calculator'
-  }
+    path: "**",
+    redirectTo: "calculator",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
